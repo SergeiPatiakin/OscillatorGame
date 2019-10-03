bf29178bf52489298173abde8527fe007af4b82f
Sergei Patiakin <sergei.patiakin@gmail.com>
2019-10-03 23:58:31 +0200

    Animation + click handler boilerplate

--- src/demo.ts
+++ src/demo.ts
@@ -1,19 +1,7 @@
-import { drawState } from "./game"
+import { setupGame } from "./game"
 
 export const doDemo = () => {
-  drawState({
-    canvasId: 'game-canvas',
-    canvasWidth: 600,
-    canvasHeight: 400,
-    centerX: 300,
-    centerY: 200,
-    particles: [{
-      x: 100,
-      y: 100,
-      r: 5,
-      color: '#ff0000',
-    }],
-  })
+  setupGame('game-canvas')
 }
 
 doDemo()
--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,6 @@
+const MAX_TIME_DELTA = 0.1 // seconds
+const RADIUS = 5
+
 interface GameState {
   canvasId: string
   canvasWidth: number
@@ -5,12 +8,44 @@ interface GameState {
   centerX: number
   centerY: number
   particles: Array<{x: number, y: number, r: number, color: string}>
+  timestamp: number
+  gameTime: number
+  mouseDown: boolean
+}
+
+export const getInitState = (canvasId: string): GameState => ({
+  canvasId,
+  canvasWidth: 600,
+  canvasHeight: 400,
+  centerX: 300,
+  centerY: 200,
+  particles: [{
+    x: 300 + 100,
+    y: 200,
+    r: RADIUS,
+    color: '#ff0000',
+  }],
+  timestamp: 0,
+  gameTime: 0,
+  mouseDown: false,
+})
+
+export function updateState(state: GameState, timestamp: number): void {
+  const gameTimeDelta = Math.min(MAX_TIME_DELTA, timestamp - state.timestamp)
+  state.timestamp = timestamp
+  state.gameTime += gameTimeDelta
+  state.particles = [{
+    x: state.centerX + 100 * Math.cos(state.gameTime),
+    y: state.centerY + 100 * Math.sin(state.gameTime),
+    r: RADIUS,
+    color: '#ff0000',
+  }]
 }
 
 export function drawState(state: GameState) {
   const {canvasId, canvasWidth, canvasHeight, centerX, centerY, particles} = state
-  var canvas = document.getElementById(canvasId) as HTMLCanvasElement
-  var ctx = canvas.getContext('2d')!
+  const canvas = document.getElementById(canvasId) as HTMLCanvasElement
+  const ctx = canvas.getContext('2d')!
   ctx.clearRect(0, 0, canvasWidth, canvasHeight)
 
   // draw stage
@@ -29,7 +64,7 @@ export function drawState(state: GameState) {
   ctx.beginPath()
   ctx.ellipse(centerX, centerY, 30, 30, 0, 0, 2*Math.PI)
   ctx.lineWidth = 1
-  ctx.strokeStyle = '#eeeeee'
+  ctx.strokeStyle = state.mouseDown ? '#eeeeee' : '#00ff00'
   ctx.stroke()
 
   particles.forEach(p => {
@@ -38,4 +73,29 @@ export function drawState(state: GameState) {
       ctx.ellipse(p.x, p.y, p.r, p.r, 0, 0, 2*Math.PI)
       ctx.fill()
   });
+}
+
+const onMouseDown = (state: GameState) => {
+  state.mouseDown = true
+}
+
+const onMouseUp = (state: GameState) => {
+  state.mouseDown = false
+}
+
+const setupListeners = (state: GameState) => {
+  const canvas = document.getElementById(state.canvasId) as HTMLCanvasElement
+  canvas.addEventListener('mousedown', () => onMouseDown(state))
+  canvas.addEventListener('mouseup', () => onMouseUp(state))
+}
+
+export const setupGame = (canvasId: string) => {
+  const state: GameState = getInitState(canvasId)
+  setupListeners(state)
+  function refresh(timestamp: number){
+    updateState(state, timestamp)
+    drawState(state)
+    window.requestAnimationFrame(refresh)
+  }
+  window.requestAnimationFrame(refresh);
 }
